4468bf675bb86fed8fc05de44b9ee4c123d6b0e7
salah21500 <mohamed.salah21500@gmail.com>
2019-02-06 00:03:46 +0200

    Laravel forms

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -4,6 +4,22 @@
     
 
 <h3>Contact page</h3>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus provident libero, earum impedit cumque, at necessitatibus officia incidunt inventore et animi facere nobis, nostrum aspernatur delectus. Porro facilis dolores sunt?</p>
-
+{!! Form::open(['url' => 'contact/submit']) !!}
+    <div class="form-group">
+        {{Form::label('name', 'Name')}}
+        {{Form::text('name', '', ['class'=> 'form-control', 'placeholder'=> 'Enter Name'])}}
+    </div>
+    <div class="form-group">
+            {{Form::label('email', 'E-Mail Address')}}
+            {{Form::text('email', '', ['class'=> 'form-control', 'placeholder'=> 'example@example.com'])}}
+    </div>
+    <div class="form-group">
+            {{Form::label('message', 'Message')}}
+            {{Form::textarea('message', '', ['class'=> 'form-control', 'placeholder'=> 'Write your message'])}}
+    </div>
+    <div>
+        {{Form::submit('Submit',['class'=>'btn btn-primary'])}}
+    </div>
+    <br>
+{!! Form::close() !!}
 @endsection
